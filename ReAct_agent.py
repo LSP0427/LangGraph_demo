0d24064033f1cae99d2da15ab85b1cdbed3de5aa
spl <liloshengpin@gmail.com>
2026-05-11 14:36:46 +0800

    snall adjust

--- ReAct_agent.py
+++ ReAct_agent.py
@@ -16,7 +16,7 @@ class AgentState(TypedDict):
 
 @tool
 def add(a: int, b:int):
-    """This is an addition function that adds 2 numbers together"""
+    """This is an addition function that adds two numbers together"""
     return a + b 
 
 @tool
